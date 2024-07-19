56d7bc663217a544bcb2b97b80498c091f1f73cd
Addyyy <agdhonde@gmail.com>
2024-07-19 17:30:58 +0530

    image update

--- backend/index.js
+++ backend/index.js
@@ -75,10 +75,24 @@ route.post("/adduserbirthdayinfo", async (req, res) => {
   res.send({ msg: "Successfully saved birthday info" });
 });
 
-route.get("/getbirthdayinfo",async(req,res)=>{
-  const data= await birthdates.find();
+route.get("/getbirthdayinfo", async (req, res) => {
+  const data = await birthdates.find();
   res.send(data);
-})
+});
+
+route.get("/search/:name", async (req, resp) => {
+  try {
+    const data = await birthdates.find({
+      $or: [
+        { Firstname: { $regex: req.params.name } },
+        { Lastname: { $regex: req.params.name } },
+      ],
+    });
+    resp.send(data);
+  } catch (error) {
+    resp.status(500).send({ error: "An error occurred while searching" });
+  }
+});
 
 app.use("/", route);
 app.listen(5000, () => {
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,39 +1,90 @@
 import React, { useEffect, useState } from "react";
 import dayjs from "dayjs";
+import { MDBInputGroup, MDBInput, MDBIcon, MDBBtn } from "mdb-react-ui-kit";
 
 function Home() {
   const [user, setUser] = useState([]);
+  const [searchquery, setSearchquery] = useState("");
   const today = dayjs().format("DD-MM-YYYY");
-  useEffect(() => {
-    data();
-  });
+
+  useEffect(
+    () => {
+      data();
+    }, // eslint-disable-next-line
+    []
+  );
   async function data() {
     let sample = await fetch("http://localhost:5000/getbirthdayinfo", {
       headers: { authorization: `bearer ${localStorage.getItem("auth")}` },
     });
     if (sample.ok) {
       let da = await sample.json();
-      da.sort((a, b) => (a.Birthdate === today ? -1 : b.Birthdate === today ? 1 : 0));
+      da.sort((a, b) =>
+        a.Birthdate === today ? -1 : b.Birthdate === today ? 1 : 0
+      );
       setUser(da);
+    }
+  }
+
+  async function search(e) {
+    setSearchquery(e.target.value);
+    let key = e.target.value;
+    if (key === "") {
+      data();
+      return;
+    }
+    const res = await fetch(`http://localhost:5000/search/${key}`, {
+      headers: {
+        authorization: `bearer ${localStorage.getItem("auth")}`,
+      },
+    });
+    const datas = await res.json();
+    console.log(datas);
+    if (datas.length <= 0) {
+      setUser(datas);
+    } else {
+      setUser(datas);
     }
   }
 
   return (
     <div className="grid-container">
+      <div className="searchbox-container">
+        <MDBInputGroup>
+          <MDBInput
+            label={<span style={{ color: "#134B70" }}>Search</span>}
+            style={{ backgroundColor: "#f0f0f0", color: "#333" }}
+            value={searchquery}
+            onChange={(e) => {
+              search(e);
+            }}
+          />
+          <MDBBtn rippleColor="light" style={{ backgroundColor: "#134B70" }}>
+            <MDBIcon icon="search" />
+          </MDBBtn>
+        </MDBInputGroup>
+      </div>
       {user.map((item, index) => (
         <div
           className="card"
           key={index}
           style={{
             backgroundColor: item.Birthdate === today ? "#134B70" : "white",
-            color: item.Birthdate === today ? "white" : "black"
+            color: item.Birthdate === today ? "white" : "black",
           }}
         >
-          <img src="/logo192.png" alt="profilepic" />
+          {item.Birthdate === today ? (
+            <img src="/birthday.png" alt="profilepic" />
+          ) : (
+            <img src="/Not Birthday.png" alt="profilepic" />
+          )}
           <h1>
             {item.Firstname} {item.Lastname}
           </h1>
           <h2>{item.Birthdate}</h2>
+          {item.Birthdate === today ? (
+            <h6 style={{ color: "pink" }}>Today is my birthday</h6>
+          ) : null}
         </div>
       ))}
     </div>
