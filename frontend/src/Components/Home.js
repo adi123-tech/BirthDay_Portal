00c860f68d0e239d81ef576cb9b6ab4d9614cccb
Addyyy <agdhonde@gmail.com>
2024-07-24 11:52:18 +0530

    Updates

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -7,7 +7,7 @@ function Home() {
   const baseURL = useApi();
   const [user, setUser] = useState([]);
   const [searchquery, setSearchquery] = useState("");
-  const today = dayjs().format("DD-MM-YYYY");
+  const today = dayjs().format("DD-MM");
 
   useEffect(
     () => {
@@ -15,15 +15,22 @@ function Home() {
     }, // eslint-disable-next-line
     []
   );
+
   async function data() {
     let sample = await fetch(`${baseURL}getbirthdayinfo`, {
       headers: { authorization: `bearer ${localStorage.getItem("auth")}` },
     });
     if (sample.ok) {
       let da = await sample.json();
-      da.sort((a, b) =>
-        a.Birthdate === today ? -1 : b.Birthdate === today ? 1 : 0
-      );
+      da.sort((a, b) => {
+        const formattedDateA = a.Birthdate.split("-").slice(0, 2).join("-");
+        const formattedDateB = b.Birthdate.split("-").slice(0, 2).join("-");
+
+        if (formattedDateA === today) return -1;
+        if (formattedDateB === today) return 1;
+        return 0;
+      });
+
       setUser(da);
     }
   }
@@ -41,11 +48,7 @@ function Home() {
       },
     });
     const datas = await res.json();
-    if (datas.length <= 0) {
-      setUser(datas);
-    } else {
-      setUser(datas);
-    }
+    setUser(datas);
   }
 
   return (
@@ -56,38 +59,40 @@ function Home() {
             label={<span style={{ color: "#134B70" }}>Search</span>}
             style={{ backgroundColor: "#f0f0f0", color: "#333" }}
             value={searchquery}
-            onChange={(e) => {
-              search(e);
-            }}
+            onChange={(e) => search(e)}
           />
           <MDBBtn rippleColor="light" style={{ backgroundColor: "#134B70" }}>
             <MDBIcon icon="search" />
           </MDBBtn>
         </MDBInputGroup>
       </div>
-      {user.map((item, index) => (
-        <div
-          className="card"
-          key={index}
-          style={{
-            backgroundColor: item.Birthdate === today ? "#134B70" : "white",
-            color: item.Birthdate === today ? "white" : "black",
-          }}
-        >
-          {item.Birthdate === today ? (
-            <img src="/birthday.png" alt="profilepic" />
-          ) : (
-            <img src="/Not Birthday.png" alt="profilepic" />
-          )}
-          <h1>
-            {item.Firstname} {item.Lastname}
-          </h1>
-          <h2>{item.Birthdate}</h2>
-          {item.Birthdate === today ? (
-            <h6 style={{ color: "pink" }}>Today is my birthday</h6>
-          ) : null}
-        </div>
-      ))}
+      {user.map((item, index) => {
+        const formattedDate = item.Birthdate.split("-").slice(0, 2).join("-");
+        return (
+          <div
+            className="card"
+            key={index}
+            style={{
+              backgroundColor: formattedDate === today ? "#134B70" : "white",
+              color: formattedDate === today ? "white" : "black",
+            }}
+          >
+            <img
+              src={
+                formattedDate === today ? "/birthday.png" : "/Not Birthday.png"
+              }
+              alt="profilepic"
+            />
+            <h1>
+              {item.Firstname} {item.Lastname}
+            </h1>
+            <h2>{item.Birthdate}</h2>
+            {formattedDate === today && (
+              <h6 style={{ color: "pink" }}>Today is my birthday</h6>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
